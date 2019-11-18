40e92d18ecf14437fa69e882c4de7655472cdc86
alfredosumosav <asumosa1993@gmail.com>
2019-11-18 12:16:25 -0800

    Creates basic structure of trip component

---
+++ frontend/src/components/trips/trip.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    fetchTrips,
+    fetchUserTrips,
+    createTrip
+} from '../../actions/trip_actions';
+
+class Trip extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return null;
+    }
+}
+
+
+export default Trip;
