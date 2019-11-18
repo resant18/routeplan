3f35a3db12c528593fab26b70f2473852c5d36ef
alfredosumosav <asumosa1993@gmail.com>
2019-11-18 12:18:31 -0800

    Adds trip container

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TripContainer from '../../components/trips/trip_container';
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>        
+      <div>
+        <TripContainer />        
         <footer>Copyright Alfredo - Lance - Renata - Timothy</footer>
       </div>
     );
