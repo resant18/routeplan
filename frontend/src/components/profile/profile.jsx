2069bbeb817e938d6f2e60309deab5d88559cef3
alfredosumosav <asumosa1993@gmail.com>
2019-11-19 09:42:46 -0800

    Refactors code and delete comments

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -4,10 +4,6 @@ import React from "react";
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   tweets: []
-    // };
   }
 
   componentWillMount() {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,18 +1,9 @@
 const express = require('express');
-// const bcrypt = require('bcryptjs');
-// const User = require('../../models/User');
-// const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
 const passport = require('passport');
+const router = express.Router();
 const usersController = require('../../controllers/users-controller');
 
-// const validateRegisterInput = require('../../validation/register');
-// const validateLoginInput = require('../../validation/login');
-
-const router = express.Router();
-
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
-
 router.get("/current", passport.authenticate('jwt', { session: false }), usersController.getCurrentUser);
 router.post("/register", usersController.registerUser);
 router.post("/login", usersController.loginUser);
