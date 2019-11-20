f5b778b784b16b2568e9ee53d4de8b6a12ed6f50
alfredosumosav <asumosa1993@gmail.com>
2019-11-19 17:05:21 -0800

    Adds trips reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 // import trips from './trips_reducer';
 import errors from './error_reducer';
+import TripsReducer from './trips_reducer';
 
 const RootReducer = combineReducers({
   session,
-  // trips,
+  trips: TripsReducer,
   errors
 });
 
