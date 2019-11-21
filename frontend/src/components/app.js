fe03bb9b0a5b5de60dcc0b12309e804ef6a5a3ba
alfredosumosav <asumosa1993@gmail.com>
2019-11-20 22:04:08 -0800

    Remove mapquest from index page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './navbar/navbar_container';
-import MapQuest from './map/map_quest';
 
 import TripFormContainer from './trip/trip_form_container';
 import MainPageContainer from './main/main_page_container';
@@ -21,15 +20,6 @@ const App = () => (
       <ProtectedRoute exact path="/trip" component={TripFormContainer} />
       <Route exact path="/" component={MainPageContainer} />
     </Switch>
-    <MapQuest
-      center={[37.7749, -122.4194]} 
-      baseLayer={"map"}
-      zoom={12}
-      routeStart={[37.798634, -122.4194]}
-      routeEnd={[37.7724, -122.4415]}
-      apiKey={"5WysS7ThGWencRAPbVcjUAV5ws0BepvA"}
-      maxMatches={500}
-      />
   </div>
 );
 
