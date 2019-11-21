7193ce9ee139b9631c197358b3b0d895ce2a7745
resant18 <santoso.renata@gmail.com>
2019-11-20 22:02:01 -0800

    Make new trip link to open modal

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
-// import '../../lib/place-search';
+
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
   logoutUser(e) {
@@ -20,7 +21,7 @@ class NavBar extends React.Component {
     if (this.props.loggedIn) {
       return (
         <div>
-          <Link to="/">New Trip</Link>
+          <button onClick={this.showModal}>New Trip</button>
           <a href="#">Menu</a>          
           <button onClick={this.logoutUser}>Logout</button>
         </div>
@@ -33,6 +34,10 @@ class NavBar extends React.Component {
         </div>
       );
     }
+  }
+
+  showModal() {
+    return () => this.props.showModal('trip-form');
   }
 
   render() {
--- frontend/src/components/navbar/navbar_container.js
+++ frontend/src/components/navbar/navbar_container.js
@@ -1,10 +1,17 @@
-import { connect } from "react-redux";
-import { logout } from "../../actions/session_actions";
-
-import NavBar from "./navbar";
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import { showModal, hideModal } from '../../actions/modal_actions';
+import NavBar from './navbar';
 
 const mapStateToProps = state => ({
   loggedIn: state.session.isAuthenticated
 });
 
-export default connect(mapStateToProps, { logout })(NavBar);
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout),
+  showModal: modal => dispatch(showModal(modal)),
+  hideModal: () => dispatch(hideModal)
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
