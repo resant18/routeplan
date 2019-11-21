b4c0dbb3cdbc56fb3fbf108f2094ee2ba39f287e
resant18 <santoso.renata@gmail.com>
2019-11-20 21:56:08 -0800

    Make  New Trip link to open modal form

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -20,7 +20,7 @@ class NavBar extends React.Component {
     if (this.props.loggedIn) {
       return (
         <div>
-          <Link to={'/'}>New Trip</Link>
+          <Link to="/">New Trip</Link>
           <a href="#">Menu</a>          
           <button onClick={this.logoutUser}>Logout</button>
         </div>
