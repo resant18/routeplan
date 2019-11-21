09d2c95f61a6f69e155a2b7e2574f6d5b03096b1
resant18 <santoso.renata@gmail.com>
2019-11-20 22:01:33 -0800

    Setup modal configuration

---
+++ frontend/src/actions/modal_actions.js
@@ -0,0 +1,15 @@
+export const SHOW_MODAL = "SHOW_MODAL";
+export const HIDE_MODAL = "HIDE_MODAL";
+
+export const showModal = modal => {
+  return {
+    type: SHOW_MODAL,
+    modal
+  };
+};
+
+export const hideModal = () => {
+  return {
+    type: HIDE_MODAL
+  };
+};
