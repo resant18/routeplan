04a16c864aa6213d77963b1ede07786c2724e74d
Lance Wong <lwong180@gmail.com>
2019-11-23 17:40:51 -0800

    fix edit trip thunk action, form for updating trip

---
+++ frontend/src/components/trip/trip_update_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { editTrip } from '../../actions/trip_actions';
+import { hideModal } from '../../actions/modal_actions';
+import TripForm from './trip_form';
+
+const mapStateToProps = state => {
+    return {
+        currentUser: state.session.user,
+        // newTrip: state.trips.new
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        editTrip: data => dispatch(editTrip(data)),
+        hideModal: () => dispatch(hideModal())
+    };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TripForm));
