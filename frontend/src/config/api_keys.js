65e08711cbdfa09b5cd605e4b7f3a3e73e6d4afb
Tim Scatterday <timothyscatterday@outlook.com>
2019-11-24 18:17:16 -0800

    Changed API keys

--- frontend/src/config/api_keys.js
+++ frontend/src/config/api_keys.js
@@ -1,5 +1,11 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./api_keys_prod");
-} else {
-  module.exports = require("./api_keys_dev");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./api_keys_prod");
+// } else {
+//   module.exports = require("./api_keys_dev");
+// }
+
+module.exports = {
+  MAP_KEY: "GtJj838k2uUMChaJbStq3F7qM0WobjXf",
+  YELP_KEY:
+    'mOp_g5UnBzGDCmW5_fpxXmCCaitoXrzlIwxKlEEL8Dvre40PECjRrMVWqNLMaZBeILGQWGEZ6KQyyATXlfgVAwHvw_UuzBSDVV-kDwGsR-fzmzkvgeSyL5xQeG3YXXYx'
+};
