a9171708a730252bbcd79be381ccb963c15b3ef0
alfredosumosav <asumosa1993@gmail.com>
2019-12-02 17:52:19 -0800

    Create user profile page

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './profile.css';
 
 
 class Profile extends React.Component {
@@ -19,10 +20,37 @@ class Profile extends React.Component {
     // this.setState({ tweets: newState.tweets });
   }
 
+  componentDidMount() {
+    this.props.requireUser(this.props.match.params.userId);
+  }
+
   render() {
+    if (this.props.users === undefined) {
+      return null;
+    }
+    const { username, email } = this.props.users;
+
     return (
-      <h1>{this.props.currentUser.handle}</h1>
-      
+      <div className="profile-wrapper">
+        <div className="profile-container">
+          
+          <div className="profile-title">
+            <h1>Your profile!</h1>
+          </div>
+
+          <div className="profile-content">
+            <div className="p-username">
+              <div className="p-label">Username:</div>
+              <div className="p-value">{ username }</div>
+            </div>
+            <div className="p-email">
+              <div className="p-label">Email:</div>
+              <div className="p-value">{ email }</div>
+            </div>
+          </div>
+
+        </div>      
+      </div>
     )
   }
 }
