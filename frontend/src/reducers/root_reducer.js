96c485a6a181bb32a5dc9f3fb14963a4472ece33
alfredosumosav <asumosa1993@gmail.com>
2019-12-02 17:52:45 -0800

    Add users slice of state to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,9 +3,11 @@ import session from './session_reducer';
 import trips from './trips_reducer';
 import ui from './ui_reducer';
 import errors from './error_reducer';
+import users from './users_reducer';
 
 
 const RootReducer = combineReducers({
+  users,
   session,
   trips,
   ui,
