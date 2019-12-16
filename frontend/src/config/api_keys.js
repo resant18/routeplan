3e9328f77ed6c0cac548a9ab938f693cfff4e93a
alfredosumosav <asumosa1993@gmail.com>
2019-12-16 13:30:00 -0800

    update keys file

--- frontend/src/config/api_keys.js
+++ frontend/src/config/api_keys.js
@@ -1,11 +1,11 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./api_keys_prod");
-} else {
-  module.exports = require("./api_keys_dev");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./api_keys_prod");
+// } else {
+//   module.exports = require("./api_keys_dev");
+// }
 
-// module.exports = {
-//   MAP_KEY: "mTgjAkaYmLIwYIArAqLEEyzYJkTegQC4",
-//   YELP_KEY:
-//     "mOp_g5UnBzGDCmW5_fpxXmCCaitoXrzlIwxKlEEL8Dvre40PECjRrMVWqNLMaZBeILGQWGEZ6KQyyATXlfgVAwHvw_UuzBSDVV-kDwGsR-fzmzkvgeSyL5xQeG3YXXYx"
-// };
+module.exports = {
+  MAP_KEY: "mTgjAkaYmLIwYIArAqLEEyzYJkTegQC4",
+  YELP_KEY:
+    "mOp_g5UnBzGDCmW5_fpxXmCCaitoXrzlIwxKlEEL8Dvre40PECjRrMVWqNLMaZBeILGQWGEZ6KQyyATXlfgVAwHvw_UuzBSDVV-kDwGsR-fzmzkvgeSyL5xQeG3YXXYx"
+};
