ac4ee81721a9208eb3f2948c698f758db4b24df1
alfredosumosav <asumosa1993@gmail.com>
2019-12-29 14:47:14 -0800

    Add poi validation

--- controllers/pois_controller.js
+++ controllers/pois_controller.js
@@ -4,11 +4,31 @@ const Trip = require("../models/Trip");
 exports.addPoiToTrip = (req, res) => {
   Trip.findById(req.body.tripId)
     .then(trip => { 
-      Trip.updateOne(
-        { _id: trip.id },
-        { $push: { pois: req.body.poi } })
-      .then(() => Trip.findById(req.body.tripId))
-      .then(trip => res.json(trip))
+      console.log(trip.pois);
+      console.log(req.body.poi);
+      let result = trip.pois.filter(poi => poi.id === req.body.poi.id);
+      console.log(result);
+
+      if (result.length === 0) {
+        console.log('hello');
+        return Trip.updateOne(
+          { _id: trip.id },
+          { $push: { pois: req.body.poi } })
+        .then(() => Trip.findById(req.body.tripId))
+        .then(trip => res.json(trip))
+      } else {
+        console.log(trip);
+        return res
+          .status(400)
+          .json({ poi: "That place already exists in your list" });
+      }
+      // console.log(req.body.poi.id);
+      // console.log(trip.pois.includes(req.body.poi));
+      // Trip.updateOne(
+      //   { _id: trip.id },
+      //   { $push: { pois: req.body.poi } })
+      // .then(() => Trip.findById(req.body.tripId))
+      // .then(trip => res.json(trip))
 
     })
     .catch(err =>
