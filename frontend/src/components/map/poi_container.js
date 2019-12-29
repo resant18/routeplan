f2147fc19e697a1825f6b22fcf4fc4c072c897ac
alfredosumosav <asumosa1993@gmail.com>
2019-12-29 15:02:36 -0800

    Add react modal and lazy yelp calls

--- frontend/src/components/map/poi_container.js
+++ frontend/src/components/map/poi_container.js
@@ -1,80 +1,86 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { addPoiToTrip } from '../../actions/poi_actions';
-import './poi.css'
-import defaultSvg from '../../assets/default-place.svg'
-const axios = require('axios');
-const qs = require('qs');
-const API_KEY = require('../../config/api_keys');
+import React, { Component } from "react";
+import ReactModal from "react-modal";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { addPoiToTrip } from "../../actions/poi_actions";
+import "./poi.css";
+import defaultSvg from "../../assets/default-place.svg";
+const axios = require("axios");
+const qs = require("qs");
+const API_KEY = require("../../config/api_keys");
 
 class PoiContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      selectedPois: []
+      selectedPois: [],
+      showModal: false
     };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.yelpCall();
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
   }
 
   yelpCall() {
     const apiKey = API_KEY.YELP_KEY;
     // Place holder for Yelp Fusion's API Key. Grab them
     // from https://www.yelp.com/developers/v3/manage_app
-    var delayInMilliseconds = 1000; //1 second
 
-    setTimeout(() => {
-      // debugger;
-      //your code to be executed after 1 second
-      axios
-        .get(
-          `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=${
-            this.props.city
-          }`,
-          {
-            headers: {
-              Authorization: `Bearer ${apiKey}`,
-              accept: "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "X-Requested-With": "XMLHttpRequest"
-            },
-            params: {
-              term: this.props.name
-            },
-            paramsSerializer: params => {
-              return qs.stringify(params);
-            }
+    axios
+      .get(
+        `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=${
+          this.props.city
+        }`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "X-Requested-With": "XMLHttpRequest"
+          },
+          params: {
+            term: this.props.name
+          },
+          paramsSerializer: params => {
+            return qs.stringify(params);
           }
-        )
-        .then(res => {
-          // debugger;
-          this.setState({ data: res.data.businesses[0] });
-        })
-        // .then(() => new Promise(resolve => setTimeout(resolve, 400)))
-        .catch(err => {
-          //console.log(err)
-        });
-    }, delayInMilliseconds);
+        }
+      )
+      .then(res => {
+        this.setState({ data: res.data.businesses[0] });
+      })
+      .catch(err => {
+        //console.log(err)
+      });
   }
 
   ratingPic() {
     // https://imgur.com/a/r89Zfjk
     const rateMapping = {
-      1: "https://i.imgur.com/188xWM4.png",
-      1.5: "https://i.imgur.com/188xWM4.png",
-      2: "https://i.imgur.com/0ju9JrY.png",
-      2.5: "https://i.imgur.com/c2tN4zA.png",
-      3: "https://i.imgur.com/mB5jFJj.png",
-      3.5: "https://i.imgur.com/qr6e6IJ.png",
-      4: "https://i.imgur.com/fU6ZjD9.png",
-      4.5: "https://i.imgur.com/FoKELFt.png",
-      5: "https://i.imgur.com/8JscCtn.png"
+      1: "https://i.imgur.com/APOFtVe.png",
+      1.5: "https://i.imgur.com/MluSqV6.png",
+      2: "https://i.imgur.com/njznajS.png",
+      2.5: "https://i.imgur.com/Fe66UB8.png",
+      3: "https://i.imgur.com/W40BlGO.png",
+      3.5: "https://i.imgur.com/3bdhkhV.png",
+      4: "https://i.imgur.com/wEnzfos.png",
+      4.5: "https://i.imgur.com/YsW2hsg.png",
+      5: "https://i.imgur.com/w66Vcol.png"
     };
 
     if (this.state.data && this.state.data.rating) {
       return rateMapping[this.state.data.rating];
     }
-    return "https://i.imgur.com/k0udNsq.png";
+    return "https://i.imgur.com/T7Rlhll.png";
   }
 
   componentDidUpdate() {
@@ -82,7 +88,7 @@ class PoiContainer extends Component {
   }
 
   componentDidMount() {
-    this.yelpCall();
+    // this.yelpCall();
   }
 
   // This function pass selected POI up to Sidebar component
@@ -106,32 +112,69 @@ class PoiContainer extends Component {
     // https://imgur.com/a/r89Zfjk
     return (
       <div>
-        <div className="poi-item">
+        <div
+          className="poi-item"
+          aria-label="Click for more info"
+          onClick={this.handleOpenModal}
+        >
           <h3>{this.props.name}</h3>
-          <img className="poi-pic" src={defaultImg}></img>
-           <div><img src={this.ratingPic()}></img></div>
+          <p>
+            {`${this.props.address}, ${this.props.city}`}
+            <br></br>
+            <i>Click for more info</i>
+          </p>
           <button onClick={this.handleAddToTrip.bind(this, this.props.poi)}>
             Add to trip
           </button>
         </div>
+
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="POI info"
+          onRequestClose={this.handleCloseModal}
+          shouldCloseOnOverlayClick={true}
+          overlayClassName="popover"
+          className="modal_content"
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              margin: "10px"
+            }}
+          >
+            <p style={{ textAlign: "center" }}>
+              <h3>{this.props.name}</h3>
+              {`${this.props.address}, ${this.props.city}`}
+              <br></br>
+              {this.state.data.display_phone}
+            </p>
+            <a href={this.state.data.url} target="_blank">
+              Yelp Page
+            </a>
+            <img className="poi-pic" src={defaultImg}></img>
+            <img src={this.ratingPic()}></img>
+          </div>
+        </ReactModal>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    tripId: ownProps.match.params.tripId,
-    poi: ownProps.poi,
-    key: ownProps.key,
-    name: ownProps.name,
-    city: ownProps.city,
-    selectedPois: ownProps.selectedPois
+  tripId: ownProps.match.params.tripId,
+  poi: ownProps.poi,
+  key: ownProps.key,
+  name: ownProps.name,
+  city: ownProps.city,
+  selectedPois: ownProps.selectedPois
 });
 
 const mapDispatchToProps = dispatch => ({
-    addPoiToTrip: (poi) => dispatch(addPoiToTrip(poi))
+  addPoiToTrip: poi => dispatch(addPoiToTrip(poi))
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PoiContainer));
-
-
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(PoiContainer)
+);
