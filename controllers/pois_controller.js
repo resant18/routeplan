ac48b48d196cfe5909c2013e13e58e9ddc024892
alfredosumosav <asumosa1993@gmail.com>
2019-12-29 16:12:11 -0800

    change status code

--- controllers/pois_controller.js
+++ controllers/pois_controller.js
@@ -19,7 +19,7 @@ exports.addPoiToTrip = (req, res) => {
       } else {
         console.log(trip);
         return res
-          .status(400)
+          .status(404)
           .json({ poi: "That place already exists in your list" });
       }
       // console.log(req.body.poi.id);
