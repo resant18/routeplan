1736eb1a09a56a22ae303a271c7b49f03328a291
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 02:49:13 -0800

    Make some routes prettier

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
-const usersController = require('../../controllers/users-controller');
+const usersController = require("../../controllers/users-controller");
 
-router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
-router.get('/:userId', usersController.hello);
-router.get("/current", passport.authenticate('jwt', { session: false }), usersController.getCurrentUser);
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get("/:userId", usersController.hello);
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  usersController.getCurrentUser
+);
 router.post("/register", usersController.registerUser);
 router.post("/login", usersController.loginUser);
 
 module.exports = router;
-
