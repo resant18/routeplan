b2ead724bb92fafb4d33d91043eac9e31c079bee
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 02:49:37 -0800

    Make util files prettier

--- frontend/src/util/trip_poi_api_util.js
+++ frontend/src/util/trip_poi_api_util.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const addPoiToTrip = (data) => {
-  return axios.post('/api/pois/new', data);
+export const addPoiToTrip = data => {
+  return axios.post("/api/pois/new", data);
 };
 
 export const removePoiFromTrip = (tripId, poiId) => {
-  return axios.delete(`/api/pois/${tripId}/${poiId}`, {tripId, poiId});
+  return axios.delete(`/api/pois/${tripId}/${poiId}`, { tripId, poiId });
 };
