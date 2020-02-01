f3f1ed10a3378b6e88e42205cc60667b2706cabd
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 02:48:59 -0800

    Make validations prettier

--- validation/trip.js
+++ validation/trip.js
@@ -1,39 +1,38 @@
-const Validator = require('validator');
-const validText = require('./valid-text');
+const Validator = require("validator");
+const validText = require("./valid-text");
 
 module.exports = function validateTripInput(data) {
-    let errors = {};
+  let errors = {};
 
-    data.name = validText(data.name) ? data.name : '';
-    data.user = validText(data.user) ? data.user : '';    
+  data.name = validText(data.name) ? data.name : "";
+  data.user = validText(data.user) ? data.user : "";
 
+  if (Validator.isMongoId(data.user)) {
+    errors.user = "Invalid user";
+  }
 
-    if (Validator.isMongoId(data.user)) {
-        errors.user = 'Invalid user';
-    }
+  if (Validator.isEmpty(data.name)) {
+    errors.text = "Name field is required";
+  }
 
-    if (Validator.isEmpty(data.name)) {
-        errors.text = 'Name field is required';
-    }
+  if (Validator.isEmpty(String(data.origin))) {
+    errors.text = "Origin field is required";
+  }
 
-    if (Validator.isEmpty(String(data.origin))) {
-      errors.text = "Origin field is required";
-    }
+  if (!Validator.isLatLong(String(data.origin))) {
+    errors.origin = "Origin field is invalid";
+  }
 
-    if (!Validator.isLatLong(String(data.origin))) {
-        errors.origin = 'Origin field is invalid';
-    }
+  if (Validator.isEmpty(String(data.destination))) {
+    errors.text = "Destination field is required";
+  }
 
-    if (Validator.isEmpty(String(data.destination))) {
-      errors.text = "Destination field is required";
-    }
+  if (!Validator.isLatLong(String(data.destination))) {
+    errors.destination = "Destination field is invalid";
+  }
 
-    if (!Validator.isLatLong(String(data.destination))) {
-        errors.destination = 'Destination field is invalid';
-    }
-
-    return {
-        errors,
-        isValid: Object.keys(errors).length === 0
-    };
-}
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
