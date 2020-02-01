9e82e6e33d5b639f3d6acd523754827e1c3fb0db
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 02:49:53 -0800

    Make some reducers prettier

--- frontend/src/reducers/users_errors_reducer.js
+++ frontend/src/reducers/users_errors_reducer.js
@@ -1,17 +1,15 @@
-import {
-    RECEIVE_USER_ERRORS
-} from "../actions/user_actions";
+import { RECEIVE_USER_ERRORS } from "../actions/user_actions";
 
 const _nullErrors = [];
 
 const UsersErrorsReducer = (state = _nullErrors, action) => {
-    Object.freeze(state);
-    switch (action.type) {
-        case RECEIVE_USER_ERRORS:
-            return action.errors;
-        default:
-            return state;
-    }
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_USER_ERRORS:
+      return action.errors;
+    default:
+      return state;
+  }
 };
 
 export default UsersErrorsReducer;
--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,17 +1,15 @@
-import {
-    RECEIVE_USER
-} from "../actions/user_actions";
+import { RECEIVE_USER } from "../actions/user_actions";
 
 const UsersReducer = (state = {}, action) => {
-    Object.freeze(state);
-    let nextState = Object.assign({}, state);
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
 
-    switch (action.type) {
-        case RECEIVE_USER:
-            return action.user;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case RECEIVE_USER:
+      return action.user;
+    default:
+      return state;
+  }
+};
 
 export default UsersReducer;
