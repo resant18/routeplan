1377d69c5be40714051e61b21701756ba34bb51e
resant18 <santoso.renata@gmail.com>
2020-06-04 10:33:07 -0700

    Add social share button with circular animation

--- frontend/src/components/trip/trip_show_detail.jsx
+++ frontend/src/components/trip/trip_show_detail.jsx
@@ -10,6 +10,7 @@ import {
 import "./trip.css";
 import gmarker from "../../assets/marker-green.png";
 import rmarker from "../../assets/marker-red.png";
+import shareIcon from "../../assets/icons/icons8-share.png";
 
 class TripShowDetail extends Component {
   constructor(props) {
@@ -49,10 +50,72 @@ class TripShowDetail extends Component {
                 </EmailShareButton>
              </div>
           </div>
+
           <br />
           <hr />
           <br />
-          <h3>My Trip</h3>
+          <div className='my-trip-header'>
+             <div className='my-trip-label'>
+                <h3>My Trip</h3>
+             </div>
+             <div className='my-trip=share'>
+                <nav className='menu'>
+                   <input type='checkbox' href='#' className='menu-open' name='menu-open' id='menu-open' />
+                   <label class='menu-open-button' for='menu-open'>
+                      <img src={shareIcon} alt='Share Trip' />
+                   </label>
+                   {/* <a href='#' className='menu-item'> */}
+                   <TwitterShareButton className='menu-item share-btn' url={shareUrl}>
+                      <TwitterIcon style={{ cursor: "pointer" }} size={32} round />
+                   </TwitterShareButton>{" "}
+                   {/* </a> */}
+                   <a href='#' class='menu-item'>
+                      <FacebookShareButton className='share-btn' url={shareUrl}>
+                         <FacebookIcon style={{ cursor: "pointer" }} size={32} round />
+                      </FacebookShareButton>
+                   </a>
+                   <a href='#' class='menu-item'>
+                      <EmailShareButton className='share-btn' url={shareUrl}>
+                         <EmailIcon style={{ cursor: "pointer" }} size={32} round />
+                      </EmailShareButton>
+                   </a>
+                </nav>
+
+                <svg xmlns='http://www.w3.org/2000/svg' version='1.1'>
+                   <defs>
+                      <filter id='shadowed-goo'>
+                         <feGaussianBlur in='SourceGraphic' result='blur' stdDeviation='10' />
+                         <feColorMatrix
+                            in='blur'
+                            mode='matrix'
+                            values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7'
+                            result='goo'
+                         />
+                         <feGaussianBlur in='goo' stdDeviation='3' result='shadow' />
+                         <feColorMatrix
+                            in='shadow'
+                            mode='matrix'
+                            values='0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2'
+                            result='shadow'
+                         />
+                         <feOffset in='shadow' dx='1' dy='1' result='shadow' />
+                         <feComposite in2='shadow' in='goo' result='goo' />
+                         <feComposite in2='goo' in='SourceGraphic' result='mix' />
+                      </filter>
+                      <filter id='goo'>
+                         <feGaussianBlur in='SourceGraphic' result='blur' stdDeviation='10' />
+                         <feColorMatrix
+                            in='blur'
+                            mode='matrix'
+                            values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7'
+                            result='goo'
+                         />
+                         <feComposite in2='goo' in='SourceGraphic' result='mix' />
+                      </filter>
+                   </defs>
+                </svg>
+             </div>
+          </div>
           <div className='trip-name'>{name}</div>
           <div className='row'>
              <div className='place-icon'>
