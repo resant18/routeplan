cd289af5e48534eedfdcca3c073ee2e5653ec3ef
Renata Santoso <santoso.renata@gmail.com>
2020-09-09 21:40:33 -0700

    Add redux dev tool extension in development env

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
+import logger from "redux-logger";
 import rootReducer from "../reducers/root_reducer";
 
-const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+// for testing env
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const configureStore = (preloadedState = {}) => {
+  const hostname = window && window.location && window.location.hostname;
+
+  // store for testing environment
+  if (/localhost/.test(hostname)) {
+     return createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)));
+  }
+  // store for production environment
+  else {
+     return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+  }
+}
 
 export default configureStore;
